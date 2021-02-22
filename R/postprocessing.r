d84ea358ad40054d06510c853a44212c76e9ed8d
Sami-Leon <52415961+Sami-Leon@users.noreply.github.com>
2021-02-22 09:29:09 -0500

    Update postprocessing.r

--- R/postprocessing.r
+++ R/postprocessing.r
@@ -8,82 +8,133 @@
 # of eigenvector of SVD analysis.
 #
 # dec - SVD decomposition of normalized matrix as returned by svd()
-# maxeigen - maximum number of eigenvectors of dec processed. Default 3.
+# maxeigen - maximum number of eigenvectors of dec processed. Default 6.
 # minCG - minimum number of clusters in which eigengenes will be clustered. Default 2.
 # maxCG - maximum number of clusters in which eigengenes will be clustered
 # minCE - minimum number of clusters in which eigenexpressions will be clustered. Default 2.
 # maxCE - maximum number of clusters in which eigenexpressions will be clustered
 #
-# Author: Rodrigo Santamaria (2007)
+# Authors: Sami Leon (2021), Rodrigo Santamaria (2007)
 #
-postprocess=function(dec, maxeigen=3,minCG=2, maxCG, minCE=2, maxCE)
-  {
-  u=dec$u          #u are eigengenes, nxc (c=min(n,m), freedom degrees)
-  v=dec$v       #v are eigenarrays, mxc
-  n=dim(u)[1]
-  m=dim(v)[1]
+postprocess=function(dec, maxeigen=6, minCG=2, maxCG, minCE=2, maxCE, n_clusters, discardFirstEigenvalue)
+{
   
+  if (length(n_clusters) == 1) {n_clusters = rep(n_clusters, 2)}
+  n_row_clusters = n_clusters[1]
+  n_column_clusters = n_clusters[2]
+  
+  if (discardFirstEigenvalue) {
+    dec$u = dec$u[, -1]
+    dec$v = dec$v[, -1]
+    
+  }
+  
+
+  
+  u=dec$u #u are eigengenes, nxc (c=min(n,m), freedom degrees)
+  v=dec$v #v are eigenarrays, mxc
+  n=dim(u)[2]
+  m=dim(v)[2]
+  
+
   dev=NULL
   max=min(n,m)
   if(maxeigen>max)
-    {
-    warning("Number of eigenvectors required exceeds freedom degrees")
+  {
+    warning("Number of eigenvectors required exceeds degrees of freedom")
     maxeigen=max
+  }
+  
+  u = u[, 1:maxeigen, drop = FALSE]
+  v = v[, 1:maxeigen, drop = FALSE]
+
+  dev$numgenes = 1:maxeigen
+  dev$numexpr = 1:maxeigen
+  
+  dev$u_withinss = NULL
+  dev$v_withinss = NULL
+
+  
+
+  
+  #Determine the best cluster decomposing by k means
+  for (j in  1:maxeigen) {
+    
+    if (is.null(n_clusters)) {
+
+      tresu=iterativeKmeans(u[,j],minimum=minCG,maximum=maxCG, choice=0.5)
+      
+    } else {
+      tresu=kmeans(u[,j], centers = n_row_clusters, 
+                   nstart = 10, iter.max = 500)
     }
     
-  dev$eigengenecluster=matrix(NA,maxeigen,n)
-  dev$eigenexprcluster=matrix(NA,maxeigen,m)
-  dev$numgenes=c(1:maxeigen)
-  dev$numexpr=c(1:maxeigen)
-
-  for(i in 1:maxeigen)
-    for(j in 1:maxeigen)
-      {
-      u1=u[,i]
-      v1=v[,j]
-      #Determine the best cluster decomposing by k means
-      tresu=iterativeKmeans(u1,minimum=minCG,maximum=maxCG, choice=0.5)
-      dev$eigengenecluster[j,]=tresu$cluster
-      dev$numgene[j]=max(tresu$cluster)
     
-      #The same for eigenarrays
-      tresu=iterativeKmeans(v1,minimum=minCE,maximum=maxCE,choice=0.5)
-      dev$eigenexprcluster[j,]=tresu$cluster
-      dev$numexpr[j]=max(tresu$cluster)
-      }
-
-  dev
+    dev$numgenes[j]=max(tresu$cluster)
+    
+    dev$u_withinss[j] = sum(tresu$withinss)
+    
   }
   
   
-  # ------------------- WITHIN VAR -------------------------
+  for(j in 1:maxeigen) {
+    
+    #The same for eigenarrays
+    
+    if (is.null(n_clusters)) {
+
+      tresu=iterativeKmeans(v[,j],minimum=minCE,maximum=maxCE,choice=0.5)
+    } else {
+      tresu=kmeans(v[,j], centers = n_column_clusters, 
+                   nstart = 10, iter.max = 500)
+    }
+    
+    dev$numexpr[j]=max(tresu$cluster)
+    
+    dev$v_withinss[j] = sum(tresu$withinss)
+  }
+  
+  # Best piecewise
+  dev$u=u[,order(dev$u_withinss), drop = FALSE]
+  dev$v=v[,order(dev$v_withinss), drop = FALSE]
+  
+  dev$numgenes = dev$numgenes[order(dev$u_withinss)]
+  dev$numexpr = dev$numexpr[order(dev$v_withinss)]
+  
+  dev
+}
+
+
+# ------------------- WITHIN VAR -------------------------
 # Within Variation of a matrix, by rows.
 # Computes the row mean and then the euclidean distance of each row to the mean.
 # The lower this value is, the higher row homogeneity of the bicluster
 # returns: the mean of the distances
-withinVar=function(x,n,m)
-  {
-  within=0
 
+withinVar=function(x) {
+  n = nrow(x)
+  m = ncol(x)
+  within=0
+  
   if(n==1)#Just one row
-    {
+  {
     within=0
-    }
+  }
   else    
-    {
+  {
     if(m==1)  #Just one column
-      {
+    {
       centroid=mean(x)
       distances=sqrt(sum((x-centroid)^2))
       within=sum(distances)/n
-      }
+    }
     else  #More than one row or column
-      {
+    {
       centroid=apply(x,2,mean)
       distances=sqrt(apply(t(centroid-t(x))^2,1,sum))
       within=sum(distances)/n
-      }
     }
-  within
   }
-  
+  within
+}
+
